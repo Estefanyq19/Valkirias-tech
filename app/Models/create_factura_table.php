10a4345ff18f751fe551bf478fcf6c6105481b48
Dania <uriasdan2003@gmail.com>
2024-04-03 23:41:18 -0600

    creacion tablas

---
+++ app/Models/create_factura_table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class create_factura_table extends Model
+{
+    use HasFactory;
+}
