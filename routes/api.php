fb2d032f92b2cf5d7d5016e219211629ea3effe5
Estefanyq19 <estefanyq1908@gmail.com>
2024-04-04 11:22:14 -0600

    Creando las rutas de departamentos y municipios

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UbicacionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,45 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Ruta para listar Departamentos
+Route::get("/index-departamento",array(
+    UbicacionController::class,
+    'indexDepartamento',
+));
 
+//Ruta para listar Municipios
+Route::get("/index-municipio",array(
+    UbicacionController::class,
+    'indexMunicipio',
+));
+
+//Rutas para listar un departamento o municipio por nombre
+Route::get("/show-departamento/{nombre}",array(
+    UbicacionController::class,
+    'showDepartamento',
+));
+Route::get("/show-municipio/{nombre}",array(
+    UbicacionController::class,
+    'showMunicipio',
+));
+
+//Ruta para agregar un nuevo departamento
+Route::post('/store/departamento',array(  
+    UbicacionController::class,
+    'storeDepartamento',
+));
+//Ruta para agregar un municipio
+Route::post('/store/municipio',array(  
+    UbicacionController::class,
+    'storeMunicipio',
+));
+//Routa para modificar municipio 
+Route::put('/update/departamento/{id}',array(  
+    UbicacionController::class,
+    'updateDepartamento',
+));
+//Ruta para actualizar municipio
+Route::put('/update/municipio/{id}',array(  
+    UbicacionController::class,
+    'updateMunicipio',
+));
